d7645cce23bb10a9e07c0312db3f995a4b2000ce
İrfan Evrens <irfanevrens@gmail.com>
2017-12-06 09:59:15 +0300

    menu elements eklendi.

--- src/SystemApi/Site/Menus.php
+++ src/SystemApi/Site/Menus.php
@@ -6,6 +6,6 @@ class Menus extends Service
 {
     public function elements($data = null)
     {
-        return $this->c->backend->get('/menus/elements', $data);
+        return $this->c->backend->get('/site/menus/elements', $data);
     }
 }
