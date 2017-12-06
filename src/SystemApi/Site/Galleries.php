ff4bf94b0594afa887d0ba0ab6b98607ddc00574
İrfan Evrens <irfanevrens@gmail.com>
2017-12-06 10:55:14 +0300

    galeri için get by id eklendi.

--- src/SystemApi/Site/Galleries.php
+++ src/SystemApi/Site/Galleries.php
@@ -9,6 +9,11 @@ class Galleries extends Service
         return $this->c->backend->get('/site/galleries/items', $data);
     }
 
+    public function getById($id)
+    {
+        return $this->c->backend->get('/site/galleries/get-by-id/' . $id);
+    }
+
     public function contents($data)
     {
         return $this->c->backend->get('/site/galleries/contents', $data);
